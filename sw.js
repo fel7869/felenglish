0edfd2260901d7804e5ca589146d56854ed6c2c3
fel7869 <1920222762@qq.com>
2026-07-12 21:25:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eg-m1-v2';
+const CACHE = 'eg-modules-v1';
 const FILES = [
   './',
   './index.html',
@@ -12,7 +12,14 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
+// 网络优先：永远拿最新文件；离线时回退缓存
 self.addEventListener('fetch', (e) => {
-  // cache-first for our app shell; always allow speech/network for everything else
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html'))));
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const cp = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp));
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
 });
